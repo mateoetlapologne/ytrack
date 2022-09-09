89aa9fcec452f62b0b4eed08cbcd9868a3e94f28
SMATEO <mateo.salvador@ynov.com>
2022-09-09 11:47:08 +0200

    import

--- isalpha/main.go
+++ isalpha/main.go
@@ -3,17 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print(isalpha(("s6a")))
+	fmt.Print(isalpha((" ")))
 }
 
 func isalpha(str string) bool {
 	res := true
-	for index, letter := range str {
-		_ = index
-		if (letter < 'a' || letter > 'z') && (letter < 'A' || letter > 'Z') && (letter < '0' || letter > '9') {
-			res = false
-		}
+	if str == " " {
+		res = true
+		return res
+	} else {
 
+		for index, letter := range str {
+			_ = index
+			if (letter < 'a' || letter > 'z') && (letter < 'A' || letter > 'Z') && (letter < '0' || letter > '9') {
+				res = false
+			}
+
+		}
+		return res
 	}
-	return res
 }
